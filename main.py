6f3f5acce97e351d25949d3c5ae48f0709e15ff4
BrenoRaeder <brenoraeder97@gmail.com>
2023-12-19 15:19:58 -0300

    enviando email com noticias

--- main.py
+++ main.py
@@ -3,6 +3,52 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from time import sleep
 import pandas as pd
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def coleta_noticia_banner(id_noticia, id_descricao):
+    noticias_banner = driver.find_elements(By.XPATH, "//div[@class='bstn-hl-wrapper']")
+
+    # alinhando tela as noticias de banner
+    driver.execute_script(f'window.scrollTo(0, 300)')
+    sleep(1)
+    driver.execute_script(f'window.scrollTo(0, 520)')
+    sleep(1)
+
+    noticias_lista = []
+    descricao_noticias_lista = []
+
+    for noticia_banner in noticias_banner:
+
+        noticias_dict = {}
+
+        id_noticia += 1
+        noticias_dict['id_noticia'] = id_noticia
+
+        titulo = noticia_banner.find_element(By.CSS_SELECTOR, ".bstn-hl-mainitem")
+        noticias_dict['titulo_noticia'] = titulo.text
+
+        subtitulos = noticia_banner.find_elements(By.CSS_SELECTOR, ".bstn-hl-relateditem")
+
+        for subtitulo in subtitulos:
+            descricao_noticias_dict = {}
+            id_descricao += 1
+            descricao_noticias_dict['id_descricao'] = id_descricao
+            descricao_noticias_dict['id_noticia'] = id_noticia
+            descricao_noticias_dict['descricao_noticia'] = subtitulo.text
+            descricao_noticias_lista.append(descricao_noticias_dict)
+
+        # print
+        path = ".//imagens_noticias//"
+        noticia_banner.screenshot(path + str(id_noticia) + 'img.png')
+
+        noticias_lista.append(noticias_dict)
+
+    return id_noticia, id_descricao, noticias_lista, descricao_noticias_lista
+
 
 def coleta_noticias(id_noticia, id_descricao):
     noticias = driver.find_elements(By.XPATH, "//div[@class='feed-post bstn-item-shape type-materia']")
@@ -38,7 +84,7 @@ def coleta_noticias(id_noticia, id_descricao):
             descricao_noticias_dict['descricao_noticia'] = resumo.text
             descricao_noticias_lista.append(descricao_noticias_dict)
 
-        #print
+        # print
         driver.execute_script(f'window.scrollTo(0, {noticia.location['y'] - 150})')
         path = ".//imagens_noticias//"
         noticia.screenshot(path + str(id_noticia) + 'img.png')
@@ -47,84 +93,94 @@ def coleta_noticias(id_noticia, id_descricao):
 
     return id_noticia, id_descricao, noticias_lista, descricao_noticias_lista
 
-def coleta_noticia_banner(id_noticia, id_descricao):
-    noticias_banner = driver.find_elements(By.XPATH, "//div[@class='bstn-hl-wrapper']")
+def envia_email_noticias(corpo_email, lista_ids):
+    msg = MIMEMultipart()
 
-    #alinhando tela as noticias de banner
-    driver.execute_script(f'window.scrollTo(0, 300)')
-    sleep(1)
-    driver.execute_script(f'window.scrollTo(0, 520)')
-    sleep(1)
+    msg['Subject'] = 'Noticias SP GE'
+    msg['From'] = 'BOT Noticias SP GE'
+    msg['To'] = 'brenoraeder97@gmail.com'
 
-    noticias_lista = []
-    descricao_noticias_lista = []
+    msg.attach(MIMEText(corpo_email))
 
-    for noticia_banner in noticias_banner:
+    for ids in lista_ids:
+        with open(f'C://Info4//WebScrapping//Estudo//estudo_webscraping_spge//imagens_noticias//{ids}img.png', 'rb') as img_file:
+            img = MIMEImage(img_file.read())
+            img.add_header('Content-Disposition', 'attachment', filename='imagem.jpg')
+            msg.attach(img)
 
-        noticias_dict = {}
+    arquivo_senha = open("C://Info4//WebScrapping//Estudo//email_teste//env.txt", "r")
+    senha = arquivo_senha.read()
 
-        id_noticia += 1
-        noticias_dict['id_noticia'] = id_noticia
+    MyServer = smtplib.SMTP('smtp.gmail.com', 587)
+    MyServer.starttls()
+    MyServer.login('t9047688@gmail.com', senha)
+    MyServer.send_message(msg)
+    MyServer.quit()
 
-        titulo = noticia_banner.find_element(By.CSS_SELECTOR, ".bstn-hl-mainitem")
-        noticias_dict['titulo_noticia'] = titulo.text
+    print('Email enviado')
 
-        subtitulos = noticia_banner.find_elements(By.CSS_SELECTOR, ".bstn-hl-relateditem")
+def verifica_noticias(noticias_df, descriao_noticias_df):
+    i = 0
+    corpo_email = ''
+    lista_ids = []
+    for noticia in noticias_df['titulo_noticia']:
+        i += 1
+        if 'Lucas' in noticia:
+            corpo_email += "Titulo da Notícia: \n" + noticia + "\n\n"
+            descricoes = descricao_noticias_df.loc[descricao_noticias_df['id_noticia'] == i]
+            for descricao in descricoes['descricao_noticia']:
+                corpo_email += "Subtiutlo: \n" + descricao + "\n"
+            lista_ids.append(i)
+            corpo_email += "\n ---\n\n"
 
-        for subtitulo in subtitulos:
-            descricao_noticias_dict = {}
-            id_descricao += 1
-            descricao_noticias_dict['id_descricao'] = id_descricao
-            descricao_noticias_dict['id_noticia'] = id_noticia
-            descricao_noticias_dict['descricao_noticia'] = subtitulo.text
-            descricao_noticias_lista.append(descricao_noticias_dict)
+    envia_email_noticias(corpo_email, lista_ids)
 
-        #print
-        path = ".//imagens_noticias//"
-        noticia_banner.screenshot(path + str(id_noticia) + 'img.png')
-
-        noticias_lista.append(noticias_dict)
-
-    return id_noticia, id_descricao, noticias_lista, descricao_noticias_lista
 
 #############
 
-#usando web driver para abrir e maximizar a páginadriver = webdriver.Chrome()
+# usando web driver para abrir e maximizar a página
 driver = webdriver.Chrome()
 driver.get('https://ge.globo.com/futebol/times/sao-paulo/')
 driver.maximize_window()
 
 sleep(3)
 
-#descendo até o final da página
+# descendo até o final da página
 for i in range(4):
     driver.execute_script('window.scrollBy(0, document.body.scrollHeight);')
     sleep(2)
 
-#fechando alerta de cookies
+# fechando alerta de cookies
 alerta_cookies = driver.find_element(By.XPATH, "//button[@class='cookie-banner-lgpd_accept-button']")
 alerta_cookies.click()
 
-#id para as noticias
+# id para as noticias
 id_noticia = 0
-#id para as descricoes
+# id para as descricoes
 id_descricao = 0
 
-id_noticia, id_descricao, lista_noticias_banner, lista_descricao_noticias_banner = coleta_noticia_banner(id_noticia, id_descricao)
+# recebendo lista de noticias e descrição
+id_noticia, id_descricao, lista_noticias_banner, lista_descricao_noticias_banner = coleta_noticia_banner(id_noticia,
+                                                                                                         id_descricao)
 id_noticia, id_descricao, lista_noticias, lista_descricao_noticias = coleta_noticias(id_noticia, id_descricao)
 
+# tranformando em df
 noticias_banner_df = pd.DataFrame(lista_noticias_banner)
 descricao_noticias_banner_df = pd.DataFrame(lista_descricao_noticias_banner)
-
 noticias_df = pd.DataFrame(lista_noticias)
 descricao_noticias_df = pd.DataFrame(lista_descricao_noticias)
 
+# concatenando os df
 noticias_df = pd.concat([noticias_banner_df, noticias_df], ignore_index=True)
 descricao_noticias_df = pd.concat([descricao_noticias_banner_df, descricao_noticias_df], ignore_index=True)
 
 print(noticias_df)
 print(descricao_noticias_df)
 
+# salvando como csv
 path = ".//tabelas_noticias//"
 noticias_df.to_csv(path + "noticias.csv", index=False)
 descricao_noticias_df.to_csv(path + "descricao_noticias.csv", index=False)
+
+# email
+verifica_noticias(noticias_df, descricao_noticias_df)
